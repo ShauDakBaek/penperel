24888e597bccdcd6c0d652dae2790e18e939a0f9
shodiqin9 <shodiqin9@gmail.com>
2023-09-12 14:39:08 +0800

    commit

---
+++ pelajar/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>Selamat datang, Pelajar!</h1>
+<br>
+<h3>Jumlah Warden : N/A</h3>
+<h3>Jumlah Pelajar : N/A</h3>
+<h3>Jumlah Peralatan : N/A</h3>
+</body>
+</html>
