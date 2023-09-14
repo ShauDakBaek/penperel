c0a87fa6aa637833aed05312921678d8a0fc80ac
Shaqiff1 <18DDT21F1086@student.ptss.edu.my>
2023-09-14 16:34:40 +0800

    shaqiff

--- warden/simpan.php
+++ warden/simpan.php
@@ -3,12 +3,23 @@ require '../include/conn.php';
 
 $namapelajar = $_POST['namapelajar'];
 $nokppelajar = $_POST['nokppelajar'];
-$katalaluan = $_POST['katalaluan'];
+$katalaluan = $_POST['nokppelajar'];
 
 $hashed = password_hash($katalaluan, PASSWORD_BCRYPT);
 
-$sql = "INSERT INTO pelajar (namapelajar, nokppelajar, kata) VALUES ($namapelajar', '$nokppelajar', '$hashed')";
-$conn->query($sql);
+$checksql = "SELECT COUNT(*) AS count FROM pelajar WHERE nokppelajar = '$nokppelajar'";
+$result = $conn->query($checksql);
+$row = $result->fetch_object();
 
-header('location: index.php?menu=senarai_pelajar');
-?>
+if ($row->count==0){
+    $sql = "INSERT INTO pelajar (namapelajar, nokppelajar, kata) VALUES ('$namapelajar', '$nokppelajar', '$hashed')";
+    $conn->query($sql);
+    header('location: index.php?menu=senarai_pelajar');
+} else {
+    ?>
+    <script>
+        alert('NoKP Pelajar teleh wujud.');
+        window.location = 'index.php?menu=senarai_pelajar';
+    </script>
+    <?php
+}
