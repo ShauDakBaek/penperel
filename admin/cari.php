457c4681aa75b1c62d71e3228ca02cd0da1c994b
ShauDakBaek <126229595+ShauDakBaek@users.noreply.github.com>
2023-09-16 14:21:23 +0800

    update cari_peralatan
    update

--- admin/cari.php
+++ admin/cari.php
@@ -1,43 +1,13 @@
 <?php
 require '../include/conn.php';
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>PenPerEl - Admin Dashboard</title>
-</head>
-<body>
-<table>
-    <tr>
-        <td>PenPerEl</td>
-        <td>
-            <a href="index.php?menu=home">Home</a>
 
-            <a href="index.php?menu=senarai_warden">Senarai Warden</a>
-
-            <a href="index.php?menu=cari_peralatan">Cari Peralatan</a>
-
-            <a href="index.php?menu=profile">Profile</a>
-
-            <a href="../logout.php">Log Keluar</a>
-        </td>
-    </tr>
-</table>
-
-<?php
-$menu = 'cari_peralatan'; #default value
+$menu = 'cari_peralatan'; # default value
 if (isset($_GET['menu'])) {
     $menu = $_GET['menu'];
 }
 include "$menu.php";
-?>
 
-<?php
-$nosiri = $_GET['nosiri'];
+$nosiri = $_POST['nosiri'];
 
 $sql = "SELECT * FROM `peralatan` WHERE `nosiri` = '$nosiri'";
 $result = $conn->query($sql);
@@ -52,7 +22,6 @@ if ($row) {
     $sql2 = "SELECT * FROM `pelajar` WHERE `idpelajar` = '$row->pelajar'";
     $result2 = $conn->query($sql2);
 
-
     if (!$result2) {
         die("Query failed: " . $conn->error);
     }
@@ -62,28 +31,8 @@ if ($row) {
     $result3 = $conn->query($sql3);
     $row3 = $result3->fetch_object();
 
-    if ($row2) {
-        ?>
-        <h3>Butiran Peralatan</h3>
-        <label>No.Siri : <?php echo $row->nosiri ?></label>
-        <br>
-        <label>Nama Warden : <?php echo $row3->namawarden ?></label>
-        <br>
-        <label>Nama Pelajar : <?php echo $row2->namapelajar ?></label>
-        <br>
-        <label>Jenis Peralatan : <?php echo $row->jenisperalatan ?></label>
-        <br>
-        <label>Jenama : <?php echo $row->jenama ?></label>
-        <?php
-    } else {
-        $namapelajar = "Pelajar not found"; // Set a default value or error message.
-    }
-
-
+    header("Location: index.php?menu=cari_peralatan&nosiri=$nosiri");
+    exit();
 } else {
     echo "<br><label><i>Peralatan bernombor siri: $nosiri tidak dijumpai.</i></label>";
 }
-?>
-
-</body>
-</html>
--- admin/cari_peralatan.php
+++ admin/cari_peralatan.php
@@ -6,9 +6,56 @@
 </head>
 <body>
 <h2>Cari Peralatan</h2>
-<form action="cari.php" method="get">
+<form action="cari.php" method="post">
     <input name="nosiri" placeholder="No. Siri Peralatan" required>
     <button type="submit">Cari</button>
 </form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['nosiri'])) {
+    $nosiri = $_GET['nosiri'];
+
+
+    $sql = "SELECT * FROM `peralatan` WHERE `nosiri` = '$nosiri'";
+    $result = $conn->query($sql);
+
+    if (!$result) {
+        die("Query failed: " . $conn->error);
+    }
+
+    $row = $result->fetch_object();
+
+    if ($row) {
+        $sql2 = "SELECT * FROM `pelajar` WHERE `idpelajar` = '$row->pelajar'";
+        $result2 = $conn->query($sql2);
+
+        if (!$result2) {
+            die("Query failed: " . $conn->error);
+        }
+
+        $row2 = $result2->fetch_object();
+        $sql3 = "SELECT namawarden FROM warden WHERE idwarden = '$row2->warden'";
+        $result3 = $conn->query($sql3);
+        $row3 = $result3->fetch_object();
+
+        ?>
+        <h3>Butiran Peralatan</h3>
+        <label>No.Siri : <?php echo $row->nosiri ?></label>
+        <br>
+        <label>Nama Warden : <?php echo $row3->namawarden ?></label>
+        <br>
+        <label>Nama Pelajar : <?php echo $row2->namapelajar ?></label>
+        <br>
+        <label>Jenis Peralatan : <?php echo $row->jenisperalatan ?></label>
+        <br>
+        <label>Jenama : <?php echo $row->jenama ?></label>
+        <?php
+    } else {
+        echo "<br><label><i>Peralatan bernombor siri: $nosiri tidak dijumpai.</i></label>";
+    }
+} else {
+
+}
+?>
 </body>
 </html>
